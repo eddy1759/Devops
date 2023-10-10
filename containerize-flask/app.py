afd1dfffaf106b2c7dfa48b904a9db45e6397a5f
Edet Asuquo <eddy1759@gmail.com>
2023-10-10 20:08:27 +0100

    feat: containerize the flask app

---
+++ containerize-flask/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__, static_url_path='/static', static_folder='static')
+
+@app.route('/')
+def hello():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
